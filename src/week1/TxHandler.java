008fc44e3268d575b9566a8749ae690a039bf975
manuel <maigfrga@gmail.com>
2018-03-26 22:07:50 +0100

    week1 task

--- src/week1/TxHandler.java
+++ src/week1/TxHandler.java
@@ -2,6 +2,7 @@ package week1;
 
 public class TxHandler {
 
+	private UTXOPool utxoPool;
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
@@ -9,6 +10,7 @@ public class TxHandler {
      */
     public TxHandler(UTXOPool utxoPool) {
         // IMPLEMENT THIS
+    	this.utxoPool = utxoPool;
     }
 
     /**
