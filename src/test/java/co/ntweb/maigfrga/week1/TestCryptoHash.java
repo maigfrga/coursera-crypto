5c361d67ae999a26968a9458a2fa31f1e346ea38
Manuel Ignacio Franco <maigfrga@gmail.com>
2019-06-09 19:15:44 +0100

    hash

---
+++ src/test/java/co/ntweb/maigfrga/week1/TestCryptoHash.java
@@ -0,0 +1,14 @@
+package co.ntweb.maigfrga.week1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCryptoHash {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
